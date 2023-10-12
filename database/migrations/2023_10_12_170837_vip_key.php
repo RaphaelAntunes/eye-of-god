160331a8dd8663463cb44c5df151480ca0228e28
RaphaelAntunes <50173113+RaphaelAntunes@users.noreply.github.com>
2023-10-12 14:13:47 -0300

    fix

--- database/migrations/2023_10_12_170837_vip_key.php
+++ database/migrations/2023_10_12_170837_vip_key.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,6 +16,13 @@ return new class extends Migration
             $table->string('data_ativo', 255); // 4 - data_ativo varchar(255)
 
         });
+
+        // Insira o registro com a chave '1234567'
+        DB::table('vip_key')->insert([
+            'chave' => '1234567',
+            'email_ativo' => null, // Preencha com o valor apropriado
+            'data_ativo' => null, // Preencha com o valor apropriado
+        ]);
     }
 
     public function down()
--- database/migrations/2023_10_12_171020_token.php
+++ database/migrations/2023_10_12_171020_token.php
@@ -3,15 +3,21 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up()
     {
         Schema::create('token', function (Blueprint $table) {
-            $table->string('token', 1024); // token varchar(1024)
+            $table->string('token', 1024); // token varchar(1024
             $table->id(); // 2 - id Primária
         });
+
+        // Insira o registro com o token '1'
+        DB::table('token')->insert([
+            'token' => '1',
+        ]);
     }
 
     public function down()
